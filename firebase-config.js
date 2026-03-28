2ec224c3d6dce82964b0208bd62483eb5930c9b3
Heraline <heralineheraline@gmail.com>
2026-03-28 10:47:37 +0800

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.__FIREBASE_CONFIG__ = {
-  apiKey: "AIzaSyBhrlxbni_T4OHTf-_VlvBtOQ0Tm5XNicE",
-  authDomain: "family-wallet-20e8b.firebaseapp.com",
-  databaseURL: "https://family-wallet-20e8b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "family-wallet-20e8b",
-  storageBucket: "family-wallet-20e8b.firebasestorage.app",
-  messagingSenderId: "70042258839",
-  appId: "1:70042258839:web:13efe2ddc3c482980e48d2"
-};
